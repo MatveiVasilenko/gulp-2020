5bd7ee900125df8be7aa5a7701ce2697dc51d06c
MatveiVasilenko <mat.vasilenkoo@gmail.com>
2020-04-22 18:57:34 +0300

    first-load04.2020

---
+++ assets/src/js/slick.js
@@ -0,0 +1,25 @@
+// // Подключение slick-slider
+// $(document).ready(function(){
+// 	$('.gallery').slick({
+// 		slidesToShow: 3,
+//   		slidesToScroll: 1,
+//   		arrows: true,
+//   		prevArrow: '<i class="fas fa-chevron-left gallery__arrow--prev"></i>',
+//   		nextArrow: '<i class="fas fa-chevron-right gallery__arrow--next"></i>',
+//   		responsive: [
+//   			{
+//   				breakpoint: 1024,
+//   				settings: {
+//   					slidesToShow: 2,
+//   				}
+//   			},
+//   			{
+//   				breakpoint: 576,
+//   				settings: {
+//   					slidesToShow: 1,
+//   					arrows: false
+//   				}
+//   			},
+//   		]
+// 	});
+// });
